4a92910dbe8b02b602d5482b4962a657bffc10a9
Tyler Lafrance <tylerlafrance@protonmail.com>
2021-03-09 13:56:54 -0500

    Add book class

---
+++ src/nolofinder/Book.java
@@ -0,0 +1,22 @@
+package nolofinder;
+
+public class Book {
+    String title;
+    int price;
+
+    void setTitle(String newTitle) {
+        title = newTitle;
+    }
+
+    void setPrice(int newPrice) {
+        price = newPrice;
+    }
+
+    String getTitle() {
+        return title;
+    }
+
+    int getPrice() {
+        return price;
+    }
+}

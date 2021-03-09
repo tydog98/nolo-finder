b6ad1becaafb51919b9e4674070ba68c6afd8583
Tyler Lafrance <tylerlafrance@protonmail.com>
2021-03-09 14:04:13 -0500

    Add course class

---
+++ src/nolofinder/Course.java
@@ -0,0 +1,18 @@
+package nolofinder;
+
+public class Course {
+    //included in bookstore CSV
+    Book[] books;
+    String courseSubject;
+    String courseNumber;
+    String courseSection;
+    int totalCost;
+    boolean nolo;
+    String instructorName;
+
+    //included in roomlist CSV
+    String crn;
+    String courseName;
+    String instructorEmail;
+
+}

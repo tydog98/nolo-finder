3ae1603f307998ef88d8ec4da965b3706c3d5e9b
Tyler Lafrance <tylerlafrance@protonmail.com>
2021-03-10 10:49:05 -0500

    Fix exception if there is no bookstore file

--- src/nolofinder/Model.java
+++ src/nolofinder/Model.java
@@ -30,43 +30,45 @@ public class Model {
     }
 
     void importBookstoreData() throws IOException, CsvValidationException {
+        //check if there is a file to work on
+        if (bookstoreFileLocation != null && !bookstoreFileLocation.equals("")) {
 
-        int currentCourse = 0; //index for the current course
-        final int SUBJECT_INDEX = 0;
-        final int DURATION_INDEX = 10;
-        final int TITLE_INDEX = 5;
-        final int PRICE_INDEX = 13;
+            int currentCourse = 0; //index for the current course
+            final int SUBJECT_INDEX = 0;
+            final int DURATION_INDEX = 10;
+            final int TITLE_INDEX = 5;
+            final int PRICE_INDEX = 13;
 
-        //reads in CSV file
-        CSVReader reader = new CSVReader(new FileReader(bookstoreFileLocation));
-        String[] nextLine; //an array of values from the line
+            //reads in CSV file
+            CSVReader reader = new CSVReader(new FileReader(bookstoreFileLocation));
+            String[] nextLine; //an array of values from the line
 
-        while ((nextLine = reader.readNext()) != null) {
+            while ((nextLine = reader.readNext()) != null) {
 
-            //if the subject code is not empty, add a new course and relevant information
-            if (!nextLine[SUBJECT_INDEX].equals("")) {
-                courses.add(new Course());
-                currentCourse = courses.size() - 1;
-                courses.get(currentCourse).setCourseSubject(nextLine[SUBJECT_INDEX]);
+                //if the subject code is not empty, add a new course and relevant information
+                if (!nextLine[SUBJECT_INDEX].equals("")) {
+                    courses.add(new Course());
+                    currentCourse = courses.size() - 1;
+                    courses.get(currentCourse).setCourseSubject(nextLine[SUBJECT_INDEX]);
 
-                //if no subject code was found, it is assumed to be a book
-                //if the duration of the book is N/A or PURCHASE, add the book
-            } else if (nextLine[DURATION_INDEX].equals("   N/A") || nextLine[DURATION_INDEX].equals("   PURCHASE")) {
-                //add the book with the title and price
-                courses.get(currentCourse).addBook(nextLine[TITLE_INDEX], nextLine[PRICE_INDEX]);
+                    //if no subject code was found, it is assumed to be a book
+                    //if the duration of the book is N/A or PURCHASE, add the book
+                } else if (nextLine[DURATION_INDEX].equals("   N/A") || nextLine[DURATION_INDEX].equals("   PURCHASE")) {
+                    //add the book with the title and price
+                    courses.get(currentCourse).addBook(nextLine[TITLE_INDEX], nextLine[PRICE_INDEX]);
+                }
+
             }
 
+            //removes the "COURSE" header from list of courses
+            courses.remove(0);
+
+            //print courses and their books for testing purposes
+            for (Course course : courses) {
+                System.out.println(course.getCourseSubject());
+                course.printBookList();
+            }
         }
-
-        //removes the "COURSE" header from list of courses
-        courses.remove(0);
-
-        //print courses and their books for testing purposes
-        for (Course course : courses) {
-            System.out.println(course.getCourseSubject());
-            course.printBookList();
-        }
-
     }
 
 }

7431f09c731838a2015eb32b7a92ec4308f37aad
Tyler Lafrance <tylerlafrance@protonmail.com>
2021-03-11 13:39:27 -0500

    Remove unused import

--- src/nolofinder/Model.java
+++ src/nolofinder/Model.java
@@ -3,7 +3,6 @@ package nolofinder;
 import com.opencsv.CSVReader;
 import com.opencsv.exceptions.CsvValidationException;
 
-import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;

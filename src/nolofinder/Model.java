77c7f53a2f5652fe6f7996205cd8599dbc4fdaf6
Tyler Lafrance <tylerlafrance@protonmail.com>
2021-04-06 13:33:24 -0400

    Add header checks and remove some workarounds

--- src/nolofinder/Model.java
+++ src/nolofinder/Model.java
@@ -39,71 +39,80 @@ public class Model {
         //check if there is a file to work on
         if (bookstoreFileLocation != null && !bookstoreFileLocation.isEmpty()) {
 
-            int currentCourse = 0; //index for the current course
-            final int SUBJECT_INDEX = 0;
-            final int DURATION_INDEX = 10;
-            final int TITLE_INDEX = 5; //is also the same location as the instructor name
-            final int PRICE_INDEX = 13;
-            final int REQUIREMENT_INDEX = 2;
-            final int SEMESTER_INDEX = 3;
-            String[] nextLine; //an array of values from the current line in the csv file
-
-
             //reads in CSV file
             CSVReader reader = new CSVReader(new FileReader(bookstoreFileLocation));
+            String[] nextLine; //an array of values from the current line in the csv file
 
-            while ((nextLine = reader.readNext()) != null) {
+            //skips first few lines to check header
+            reader.readNext();
+            reader.readNext();
+            reader.readNext();
 
-                //if the subject code is not empty, add a new course and relevant information
-                if (!nextLine[SUBJECT_INDEX].isEmpty()) {
-                    courses.add(new Course());
-                    currentCourse = courses.size() - 1;
+            nextLine = reader.readNext();
 
-                    //make sure it's an actual course number
-                    //I spent too long figuring out why I was getting out of bounds exceptions
-                    if (!nextLine[SUBJECT_INDEX].equals("COURSE")) {
+            //check the header to make sure this is a bookstore file
+            if (nextLine[1].equals("STORE 0971:NASHUA COMMUNITY COLLEGE BOOKSTORE")) {
 
-                        //splits string by spaces to separate subject code and course number
-                        //replaces ( with a space to remove (All sections)
-                        String[] split = nextLine[SUBJECT_INDEX].trim().replace("(", " ").split(" ");
+                int currentCourse = 0; //index for the current course
+                final int SUBJECT_INDEX = 0;
+                final int DURATION_INDEX = 10;
+                final int TITLE_INDEX = 5; //is also the same location as the instructor name
+                final int PRICE_INDEX = 13;
+                final int REQUIREMENT_INDEX = 2;
+                final int SEMESTER_INDEX = 3;
 
-                        courses.get(currentCourse).setCourseSubject(split[0]);
-                        courses.get(currentCourse).setCourseNumber(split[1]);
-                        //completely removes any lingering "All"s that were connected to other characters
-                        //without a space
-                        courses.get(currentCourse).setCourseSection(split[2].replaceAll("[aA]ll", ""));
+                //skips headers and goes straight to the data
+                reader.readNext();
+                reader.readNext();
+                reader.readNext();
 
-                        //if a course section is empty it's because it was stored in the CSV as
-                        // "All Sections" and was thus removed, this replaces it with "A" (meaning all sections)
-                        if (courses.get(currentCourse).getCourseSection().isEmpty()) {
-                            courses.get(currentCourse).setCourseSection("A");
-                        }
+                while ((nextLine = reader.readNext()) != null) {
 
+                    //if the subject code is not empty, add a new course and relevant information
+                    if (!nextLine[SUBJECT_INDEX].isEmpty()) {
+                        courses.add(new Course());
+                        currentCourse = courses.size() - 1;
+
+                            //splits string by spaces to separate subject code and course number
+                            //replaces ( with a space to remove (All sections)
+                            String[] split = nextLine[SUBJECT_INDEX].trim()
+                                    .replace("(", " ").split(" ");
+
+                            courses.get(currentCourse).setCourseSubject(split[0]);
+                            courses.get(currentCourse).setCourseNumber(split[1]);
+                            //completely removes any lingering "All"s that were connected to other characters
+                            //without a space
+                            courses.get(currentCourse).setCourseSection(split[2]
+                                    .replaceAll("[aA]ll", ""));
+
+                            //if a course section is empty it's because it was stored in the CSV as
+                            // "All Sections" and was thus removed, this replaces it with "A" (meaning all sections)
+                            if (courses.get(currentCourse).getCourseSection().isEmpty()) {
+                                courses.get(currentCourse).setCourseSection("A");
+                            }
+
+                        //if no subject code was found, it is assumed to be a book
+                        //adds all books in course that can be purchased
+                    } else if (nextLine[DURATION_INDEX].trim().equals("N/A")
+                            || nextLine[DURATION_INDEX].trim().equals("PURCHASE")) {
+
+                        //add the book with the title, price, and it's requirement type
+                        //price removes the dollar sign to make casting to int easier for calculations
+                        courses.get(currentCourse).addBook(nextLine[PRICE_INDEX].replace("$", "")
+                                , nextLine[REQUIREMENT_INDEX]);
+
+                        //instructor names are on the same line as the listed semester, so if there's a semester
+                        //there's an instructor
+                        //makes sure that there's a course to add the instructor name to first
+                    } else if (!nextLine[SEMESTER_INDEX].isEmpty() && !courses.isEmpty()) {
+
+                        //instructor name is stored at same index as the book titles
+                        courses.get(currentCourse).setInstructorName(nextLine[TITLE_INDEX]
+                                .replace(",", " "));
                     }
 
-                    //if no subject code was found, it is assumed to be a book
-                    //adds all books in course that can be purchased
-                } else if (nextLine[DURATION_INDEX].trim().equals("N/A")
-                        || nextLine[DURATION_INDEX].trim().equals("PURCHASE")) {
-
-                    //add the book with the title, price, and it's requirement type
-                    //price removes the dollar sign to make casting to int easier for calculations
-                    courses.get(currentCourse).addBook(nextLine[PRICE_INDEX].replace("$", "")
-                            , nextLine[REQUIREMENT_INDEX]);
-
-                    //instructor names are on the same line as the listed semester, so if there's a semester
-                    //there's an instructor
-                    //makes sure that there's a course to add the instructor name to first
-                } else if (!nextLine[SEMESTER_INDEX].isEmpty() && !courses.isEmpty()) {
-
-                    //instructor name is stored at same index as the book titles
-                    courses.get(currentCourse).setInstructorName(nextLine[TITLE_INDEX].replace(",", " "));
                 }
-
             }
-
-            //removes the "COURSE" header from list of courses
-            courses.remove(0);
 
         }
     }
